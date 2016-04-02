cd16040f8eb780f5e7d240cb98b7c30df1f6dfeb
capricornlg <capricornlg@163.com>
2016-04-02 19:46:37 +0800

    initi

--- src/com/primeton/abstractfac/factory/CarFactory.java
+++ src/com/primeton/abstractfac/factory/CarFactory.java
@@ -2,6 +2,12 @@ package com.primeton.abstractfac.factory;
 
 /**
  * 抽象工厂
+ * JDK中Calendar的getInstance方法
+ * JDBC中的Connection对象的获取
+ * Hibernate中的SessionFactory创建Session
+ * Spring中的IOC容器创建管理bean对象
+ * XML解析时的DocumentBuilderFactory创建解析器对象
+ * 反射中Class对象的newInstance()
  * Created by Administrator on 16-4-2.
  */
 public interface CarFactory {
